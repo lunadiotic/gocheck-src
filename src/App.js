8e441260eb41f1697c1346006c8fc2a94cf47c31
aim <ahmadhakim404@gmail.com>
2023-07-08 14:35:33 +0700

    membuat layout halaman project gocheck

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <CheckList />
+      <Stats />
     </div>
-  );
+  )
 }
 
-export default App;
+function Logo() {
+  return <span className="logo">📝 GoCheck ✅  </span>
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>Ada yang mau kamu catat? 🤔</h3>
+    </div>
+  )
+}
+
+function CheckList() {
+  return <div className="list">List</div>
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <span>🗒️ Kamu punya x catatan dan baru x yg dichecklist (x%) ✅</span>
+    </footer>
+  )
+}
