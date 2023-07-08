920d530faaa368c07bc321f8d8d7d4afa22d753a
aimeliala <idstack@outlook.co.id>
2023-07-09 02:25:47 +0700

    mengendalikan data dari sebuah element

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const listItems = [
   { id: 1, title: "Eat", done: false },
   { id: 2, title: "Sleep", done: true },
@@ -11,26 +13,47 @@ export default function App() {
       <CheckList />
       <Stats />
     </div>
-  )
+  );
 }
 
 function Logo() {
-  return <span className="logo">📝 GoCheck ✅  </span>
+  return <span className="logo">📝 GoCheck ✅ </span>;
 }
 
 function Form() {
+  const [title, setTitle] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e)
+    // console.log(e);
+
+    if (!title) return;
+
+    const newItem = {
+      title,
+      done: false,
+    };
+    console.log(newItem);
+
+    setTitle("");
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>Ada yang mau kamu catat? 🤔</h3>
-      <input type="text" name="" id="" />
+      <input
+        type="text"
+        name=""
+        id=""
+        value={title}
+        onChange={(e) => {
+          console.log(e.target);
+          setTitle(e.target.value);
+        }}
+      />
       <button>add</button>
     </form>
-  )
+  );
 }
 
 function CheckList() {
@@ -42,16 +65,18 @@ function CheckList() {
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 function Item({ item }) {
   return (
     <li>
-      <span style={item.done ? { textDecoration: 'line-through' } : {}}>{item.title}</span>
+      <span style={item.done ? { textDecoration: "line-through" } : {}}>
+        {item.title}
+      </span>
       <button>❌</button>
     </li>
-  )
+  );
 }
 
 function Stats() {
@@ -59,5 +84,5 @@ function Stats() {
     <footer className="stats">
       <span>🗒️ Kamu punya x catatan dan baru x yg dichecklist (x%) ✅</span>
     </footer>
-  )
+  );
 }
