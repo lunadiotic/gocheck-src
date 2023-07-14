389db5038e8ca82a89bd352fbf61ac103220650c
aim <ahmadhakim404@gmail.com>
2023-07-14 23:34:32 +0700

    mengurutkan nilai item dari state

--- src/App.js
+++ src/App.js
@@ -79,17 +79,43 @@ function Form({ onAddItem }) {
 }
 
 function CheckList({ items, onDeleteItem, onToggleItem }) {
+	const [sortBy, setSortBy] = useState('input');
+
+	const sortItems = () => {
+		switch (sortBy) {
+			case 'input':
+				return items;
+			case 'title':
+				return items.slice().sort((a, b) => a.title.localeCompare(b.title));
+			case 'status':
+				return items.slice().sort((a, b) => Number(a.done) - Number(b.done));
+			default:
+				return items;
+		}
+	};
+
+	const sortedItems = sortItems();
+
 	return (
 		<div className='list'>
 			<ul>
-				{items.map((item) => (
+				{sortedItems.map((item) => (
 					<Item
 						item={item}
 						onDeleteItem={onDeleteItem}
 						onToggleItem={onToggleItem}
+						key={item.id}
 					/>
 				))}
 			</ul>
+
+			<div className='actions'>
+				<select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+					<option value='input'>Urutkan berdasarkan input</option>
+					<option value='title'>Urutkan berdasarkan deskripsi</option>
+					<option value='status'>Urutkan berdasarkan status</option>
+				</select>
+			</div>
 		</div>
 	);
 }
