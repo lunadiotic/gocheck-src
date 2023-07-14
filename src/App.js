937d304ac93cd0ff7c735a1907218ed987fe1b77
aimeliala <idstack@outlook.co.id>
2023-07-14 21:14:33 +0700

    ubah item: operasi immutable data

--- src/App.js
+++ src/App.js
@@ -11,11 +11,23 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItem={handleAddItem} />
-      <CheckList items={items} onDeleteItem={handleDeleteItem} />
+      <CheckList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
@@ -65,21 +77,30 @@ function Form({ onAddItem }) {
   );
 }
 
-function CheckList({ items, onDeleteItem }) {
+function CheckList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} onDeleteItem={onDeleteItem} />
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.done}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.done ? { textDecoration: "line-through" } : {}}>
         {item.title}
       </span>
