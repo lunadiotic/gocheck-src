68a8af38405994b36204514d0dd49d053530ecfd
aim <ahmadhakim404@gmail.com>
2023-07-14 22:32:48 +0700

    menghitung total item dan persentasenya dengan derived state

--- src/App.js
+++ src/App.js
@@ -1,118 +1,135 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function App() {
-  const [items, setItems] = useState([]);
+	const [items, setItems] = useState([]);
+	// const [numItems, setNumItems] = useState(0);
 
-  function handleAddItem(item) {
-    setItems((items) => [...items, item]);
-  }
+	function handleAddItem(item) {
+		setItems((items) => [...items, item]);
+	}
 
-  function handleDeleteItem(id) {
-    setItems((items) => items.filter((item) => item.id !== id));
-  }
+	function handleDeleteItem(id) {
+		setItems((items) => items.filter((item) => item.id !== id));
+	}
 
-  function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, done: !item.done } : item
-      )
-    );
-  }
+	function handleToggleItem(id) {
+		setItems((items) =>
+			items.map((item) =>
+				item.id === id ? { ...item, done: !item.done } : item
+			)
+		);
+	}
 
-  return (
-    <div className="app">
-      <Logo />
-      <Form onAddItem={handleAddItem} />
-      <CheckList
-        items={items}
-        onDeleteItem={handleDeleteItem}
-        onToggleItem={handleToggleItem}
-      />
-      <Stats />
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Logo />
+			<Form onAddItem={handleAddItem} />
+			<CheckList
+				items={items}
+				onDeleteItem={handleDeleteItem}
+				onToggleItem={handleToggleItem}
+			/>
+			<Stats items={items} />
+		</div>
+	);
 }
 
 function Logo() {
-  return <span className="logo">📝 GoCheck ✅ </span>;
+	return <span className='logo'>📝 GoCheck ✅ </span>;
 }
 
 function Form({ onAddItem }) {
-  const [title, setTitle] = useState("");
-  // const [items, setItems] = useState([]);
+	const [title, setTitle] = useState('');
+	// const [items, setItems] = useState([]);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    // console.log(e);
+	function handleSubmit(e) {
+		e.preventDefault();
+		// console.log(e);
 
-    if (!title) return;
+		if (!title) return;
 
-    const newItem = {
-      id: Date.now(),
-      title,
-      done: false,
-    };
-    console.log(newItem);
+		const newItem = {
+			id: Date.now(),
+			title,
+			done: false,
+		};
+		console.log(newItem);
 
-    onAddItem(newItem);
+		onAddItem(newItem);
 
-    setTitle("");
-  }
+		setTitle('');
+	}
 
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>Ada yang mau kamu catat? 🤔</h3>
-      <input
-        type="text"
-        name=""
-        id=""
-        value={title}
-        onChange={(e) => {
-          console.log(e.target);
-          setTitle(e.target.value);
-        }}
-      />
-      <button>add</button>
-    </form>
-  );
+	return (
+		<form className='add-form' onSubmit={handleSubmit}>
+			<h3>Ada yang mau kamu catat? 🤔</h3>
+			<input
+				type='text'
+				name=''
+				id=''
+				value={title}
+				onChange={(e) => {
+					console.log(e.target);
+					setTitle(e.target.value);
+				}}
+			/>
+			<button>add</button>
+		</form>
+	);
 }
 
 function CheckList({ items, onDeleteItem, onToggleItem }) {
-  return (
-    <div className="list">
-      <ul>
-        {items.map((item) => (
-          <Item
-            item={item}
-            onDeleteItem={onDeleteItem}
-            onToggleItem={onToggleItem}
-          />
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className='list'>
+			<ul>
+				{items.map((item) => (
+					<Item
+						item={item}
+						onDeleteItem={onDeleteItem}
+						onToggleItem={onToggleItem}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 function Item({ item, onDeleteItem, onToggleItem }) {
-  return (
-    <li>
-      <input
-        type="checkbox"
-        value={item.done}
-        onChange={() => onToggleItem(item.id)}
-      />
-      <span style={item.done ? { textDecoration: "line-through" } : {}}>
-        {item.title}
-      </span>
-      <button onClick={() => onDeleteItem(item.id)}>❌</button>
-    </li>
-  );
+	return (
+		<li>
+			<input
+				type='checkbox'
+				value={item.done}
+				onChange={() => onToggleItem(item.id)}
+			/>
+			<span style={item.done ? { textDecoration: 'line-through' } : {}}>
+				{item.title}
+			</span>
+			<button onClick={() => onDeleteItem(item.id)}>❌</button>
+		</li>
+	);
 }
 
-function Stats() {
-  return (
-    <footer className="stats">
-      <span>🗒️ Kamu punya x catatan dan baru x yg dichecklist (x%) ✅</span>
-    </footer>
-  );
+function Stats({ items }) {
+	if (!items.length) {
+		return (
+			<footer className='stats'>
+				<span>📝 Yuk mulai bikin catatan!</span>
+			</footer>
+		);
+	}
+
+	const numItems = items.length;
+	const doneItems = items.filter((item) => item.done).length;
+	const percentage = Math.round((doneItems / numItems) * 100);
+
+	return (
+		<footer className='stats'>
+			<span>
+				{percentage === 100
+					? '✅ Kamu sudah melakukannya semua'
+					: `🗒️ Kamu punya ${numItems} catatan dan baru ${doneItems} yg dichecklist (${percentage}%)`}
+			</span>
+		</footer>
+	);
 }
